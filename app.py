2196d65f45865fa6f98210e8a16e4c8e346268d3
Dzenan Kosuta <dzenan_kosuta@hotmail.com>
2022-02-26 00:24:07 +0100

    small changes

--- app.py
+++ app.py
@@ -12,21 +12,23 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///data.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "jose"
 api = Api(app)
+# It is better to init app here
 db.init_app(app)
-
 
 
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(StoreList, '/stores')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)  # important to mention debug=True
